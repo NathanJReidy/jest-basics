a5e7cba6a518d996770bb5722b2f90728996ee76
Nathan Reidy <nathanjreidy@gmail.com>
2021-10-04 22:56:07 +1100

    Added more tests to array-analysis

--- array-analysis.test.js
+++ array-analysis.test.js
@@ -8,3 +8,19 @@ test("takes an array of numbers and returns an object with average, min, max and
     length: 6,
   });
 });
+
+test("takes an array of numbers and returns average value", () => {
+  expect(arrayAnalysis([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test("takes an array of numbers and returns min value", () => {
+  expect(arrayAnalysis([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test("takes an array of numbers and returns max value", () => {
+  expect(arrayAnalysis([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test("takes an array of numbers and returns length of array", () => {
+  expect(arrayAnalysis([1, 8, 3, 4, 2, 6]).length).toBe(6);
+});
